979c5cba35273ed60a4a2129d19f19ef446a51b0
Michael Schrijver <michael.schrijver@eu.equinix.com>
2015-06-30 15:27:09 +0200

    Factor out logging into separate classes for file/syslog.

--- ldap2cef.py
+++ ldap2cef.py
@@ -27,6 +27,42 @@ class LDAPConnection(object):
         self.last_op = None
         self.op_subject = None
 
+class LDAPLogger(object):
+    EVENT_BIND = 1
+    EVENT_MODIFY = 2
+    EVENT_ADD = 3
+    EVENT_DELETE = 4
+    EVENT_NAMES = {EVENT_BIND: 'BIND', EVENT_MODIFY: 'MODIFY', EVENT_ADD: 'ADD', EVENT_DELETE: 'DELETE'}
+
+    def format_message(self, connection_id, event_id, connection, attributes):
+        return """CEF:0|mozilla|openldap|1.0|{event_id}|{event_name}|6|src={src} spt={spt} suser={user} cs1=\"{bind_name}\" cs1Label=BindDN  outcome={outcome} cs2=\"{subject_dn}\" cs2Label=SubjectDN cn1={conn_id} cn1Label=ConnId cn2={err} cn2Label=LdapCode end={end}\n""".format(
+                conn_id = connection_id,
+                event_id = event_id,
+                event_name = self.EVENT_NAMES.get(event_id, ''),
+                err = attributes['err'],
+                outcome = attributes['outcome'],
+                src = attributes['src'],
+                spt = attributes['spt'],
+                bind_name = connection.bind_dn,
+                subject_dn = connection.op_subject,
+                user = connection.bind_dn,
+                end = str(time.time())
+                )
+
+class FileLogger(LDAPLogger):
+    def __init__(self, filename):
+        self._filename = filename
+    def __call__(self, connection_id, event_id, connection, attributes):
+        # CEF:Version|Device Vendor|Device Product|Device Version|Signature ID|Name|Severity|[Extension]
+        with open(self._filename, "a") as outputfile:
+            outputfile.write(self.format_message(connection_id, event_id, connection, attributes))
+
+class SyslogLogger(LDAPLogger):
+    def __init__(self, ident):
+        syslog.openlog(ident)
+    def __call__(self, connection_id, event_id, connection, attributes):
+        syslog.syslog(self.format_message(connection_id, event_id, connection, attributes))
+
 class LDAPProcessor(object):
     """Main processor, process_message will get called for each line of stdin
     
@@ -39,43 +75,22 @@ class LDAPProcessor(object):
     ldap_attributes_re = re.compile(' (?P<key>[^ =]+)(?:=(?P<value>"([^"\\\\]|\\\\.)*"|\S+))?')
     quoted_re = re.compile('\\\\(.)')
 
-    def __init__(self):
-        self._connections = repoze.lru.ExpiringLRUCache(self.LRU_CONN_CACHE_SIZE, self.LRU_CONN_CACHE_TIMEOUT)
-
-    EVENT_BIND = 1
-    EVENT_MODIFY = 2
-    EVENT_ADD = 3
-    EVENT_DELETE = 4
-    EVENT_NAMES = {EVENT_BIND: 'BIND', EVENT_MODIFY: 'MODIFY', EVENT_ADD: 'ADD', EVENT_DELETE: 'DELETE'}
-
     OUTCOME_FAILURE = 'failure'
     OUTCOME_SUCCESS = 'success'
+
+    def __init__(self, logger):
+        self._connections = repoze.lru.ExpiringLRUCache(self.LRU_CONN_CACHE_SIZE, self.LRU_CONN_CACHE_TIMEOUT)
+        self._logger = logger
 
     def cef_log(self, connection_id, event_id, connection, attributes):
         """Will get called if we have a matching log"""
         err = attributes.get('err', '')
-        outcome = self.OUTCOME_SUCCESS if err == '0' else self.OUTCOME_FAILURE
+        attributes['outcome'] = self.OUTCOME_SUCCESS if err == '0' else self.OUTCOME_FAILURE
         try:
-            src, spt = connection.address.split(':')
+            attributes['src'], attributes['spt'] = connection.address.split(':')
         except ValueError:
-            src = spt = ''
-
-        # XXX
-        # CEF:Version|Device Vendor|Device Product|Device Version|Signature ID|Name|Severity|[Extension]
-        with open(OUTFILE, "a") as outputfile:
-            outputfile.write("""CEF:0|mozilla|openldap|1.0|{event_id}|{event_name}|6|src={src} spt={spt} suser={user} cs1=\"{bind_name}\" cs1Label=BindDN  outcome={outcome} cs2=\"{subject_dn}\" cs2Label=SubjectDN cn1={conn_id} cn1Label=ConnId cn2={err} cn2Label=LdapCode end={end}\n""".format(
-                conn_id = connection_id,
-                event_id = event_id,
-                event_name = self.EVENT_NAMES.get(event_id, ''),
-                err = err,
-                outcome = outcome,
-                src = src,
-                spt = spt,
-                bind_name = connection.bind_dn,
-                subject_dn = connection.op_subject,
-                user = connection.bind_dn,
-                end = str(time.time())
-                ))
+            attributes['src'] = attributes['spt'] = ''
+        self._logger(connection_id, event_id, connection, attributes)
 
     def process_message(self, server, message):
         """Line by line call"""
@@ -114,13 +129,13 @@ class LDAPProcessor(object):
                             if attributes['err'] == '0':
                                 # bind succeeded, assume new identity
                                 connection.bind_dn = connection.new_bind_dn
-                            self.cef_log(connection_id, self.EVENT_BIND, connection, attributes)
+                            self.cef_log(connection_id, LDAPLogger.EVENT_BIND, connection, attributes)
                         elif connection.last_op == 'DEL':
-                            self.cef_log(connection_id, self.EVENT_DELETE, connection, attributes)
+                            self.cef_log(connection_id, LDAPLogger.EVENT_DELETE, connection, attributes)
                         elif connection.last_op == 'ADD':
-                            self.cef_log(connection_id, self.EVENT_ADD, connection, attributes)
+                            self.cef_log(connection_id, LDAPLogger.EVENT_ADD, connection, attributes)
                         elif connection.last_op == 'MOD':
-                            self.cef_log(connection_id, self.EVENT_MODIFY, connection, attributes)
+                            self.cef_log(connection_id, LDAPLogger.EVENT_MODIFY, connection, attributes)
                     elif command == 'UNBIND':
                         connection.bind_dn = None
 
@@ -134,7 +149,7 @@ if __name__ == '__main__':
     """Strip date and get message, forward to process_message when there is still stdin"""
     # Jun 23 08:34:59 aa-ldap-aaaa2 slapd[2197]: conn=1022
     ldap_syslog_re = re.compile('[a-z]{3} +\d+ \d{2}:\d{2}:\d{2} (?P<server>[\w-]+) slapd\[\d+\]: (?P<message>.*)', re.I)
-    processor = LDAPProcessor()
+    processor = LDAPProcessor(FileLogger(OUTFILE))
     while True:
         line = sys.stdin.readline().rstrip()
         if line == '': break
